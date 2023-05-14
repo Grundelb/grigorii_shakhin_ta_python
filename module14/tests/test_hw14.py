ca8dfb3a4399b82795ec9039dcf1fd9463e68b55
Grigorii_Shakhin <grigorii_shakhin@epam.com>
2023-05-14 23:06:51 +0200

    completed test_add_monitor_with_highest_price

--- module14/tests/test_hw14.py
+++ module14/tests/test_hw14.py
@@ -17,4 +17,17 @@ def test_login():
     test_page.tear_down()
 
 def test_add_monitor_with_highest_price():
-    pass
+    test_page = DemoblizeHomePage()
+    test_page.set_up()
+    test_page.open_home_page()
+    test_page.open_log_in_form()
+    test_page.log_in()
+    test_page.open_monitors_category()
+    
+    assert test_page.get_highest_price_monitor() == test_page.get_highest_price_monitor_card_info()
+    
+    test_page.add_to_cart()
+    
+    assert test_page.highest_price_element[0] == test_page.open_cart()
+
+    test_page.tear_down()
