e6507f1b412e8074ec025d525161a7509a1ef582
Grigorii_Shakhin <grigorii_shakhin@epam.com>
2023-05-14 11:45:51 +0200

    completed test_login

--- module14/tests/test_hw14.py
+++ module14/tests/test_hw14.py
@@ -9,4 +9,12 @@ def test_login():
     assert test_page.get_login().is_displayed()
     assert test_page.get_password().is_displayed()
 
+    test_page.log_in()
+
+    assert test_page.get_logout_button().is_displayed()
+    assert test_page.get_welcome_message().text == f'Welcome {test_page.user_login}'
+
     test_page.tear_down()
+
+def test_add_monitor_with_highest_price():
+    pass
