a6733045d87cbd6fa07343d2a00b9958b8da81a5
Grigorii_Shakhin <grigorii_shakhin@epam.com>
2023-05-20 18:05:08 +0200

    change hw14

--- module14/tests/test_hw14.py
+++ module14/tests/test_hw14.py
@@ -1,27 +1,37 @@
 from module14.common.hw14 import DemoblizeHomePage
+from selenium import webdriver
+import pytest
 
-def test_login():
-    test_page = DemoblizeHomePage()
-    test_page.set_up()
+@pytest.fixture(scope='function')
+def driver():
+    _driver = webdriver.Chrome()
+    _driver.maximize_window()
+    yield _driver
+    _driver.quit()
+
+def test_login(driver):
+    test_page = DemoblizeHomePage(driver)
     test_page.open_home_page()
     test_page.open_log_in_form()
 
-    assert test_page.get_login().is_displayed()
-    assert test_page.get_password().is_displayed()
+    assert test_page.get_login().is_displayed(), 'Login field is not displayed'
+    assert test_page.get_password().is_displayed(), 'Password field is not displayed'
 
     test_page.log_in()
 
-    assert test_page.get_logout_button().is_displayed()
+    assert test_page.get_logout_button().is_displayed(), 'Logout button is not displayed'
     assert test_page.get_welcome_message().text == f'Welcome {test_page.user_login}'
 
-    test_page.tear_down()
-
-def test_add_monitor_with_highest_price():
-    test_page = DemoblizeHomePage()
-    test_page.set_up()
+@pytest.fixture
+def login(driver):
+    test_page = DemoblizeHomePage(driver)
     test_page.open_home_page()
     test_page.open_log_in_form()
     test_page.log_in()
+
+def test_add_monitor_with_highest_price(driver, login):
+    login
+    test_page = DemoblizeHomePage(driver)
     test_page.open_monitors_category()
     
     assert test_page.get_highest_price_monitor() == test_page.get_highest_price_monitor_card_info()
@@ -29,5 +39,3 @@ def test_add_monitor_with_highest_price():
     test_page.add_to_cart()
     
     assert test_page.highest_price_element[0] == test_page.open_cart()
-
-    test_page.tear_down()
